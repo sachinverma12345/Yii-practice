b6787171b5da2d1f62ba945fcc609efb531c3922
Sachin Verma <vermamanish1666@gmail.com>
2025-09-21 11:59:47 +0530

    make a components

--- components/CommonComponent.php
+++ components/CommonComponent.php
@@ -1,5 +1,7 @@
 <?php
 namespace app\components;
+
+use Yii;
 use yii\base\Component;
 
 class CommonComponent extends Component{
@@ -7,4 +9,9 @@ class CommonComponent extends Component{
         return "asdgfsd";
     }
 
+     public function getData(){
+        $data = Yii::$app->db->createCommand('select * from surveys')->queryAll();
+        return $data;
+        return "asdgfsd";
+    }
 }
